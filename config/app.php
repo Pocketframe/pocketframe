28067a89c21cabac42b3b14ee3207318eb0605ff
Asaba William <asabawilliamdk@yahoo.com>
2025-02-07 04:02:18 +0300

    config: add new env values

--- config/app.php
+++ config/app.php
@@ -1,5 +1,11 @@
 <?php
+require BASE_PATH . '/vendor/pocketframe/framework/src/functions.php';
 
 return [
-    date_default_timezone_set("Africa/Kampala")
+    date_default_timezone_set("Africa/Kampala"),
+
+    'app_key' => env('APP_KEY', 'your-secret-key'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+
 ];
