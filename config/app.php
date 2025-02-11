a0208f1b749d9e90e0384bbac13ad2e7d3489e92
Asaba William <asabawilliamdk@yahoo.com>
2025-02-11 12:24:23 +0300

    refactor: add timezone to keys and align arrays

--- config/app.php
+++ config/app.php
@@ -1,11 +1,12 @@
 <?php
+
 require BASE_PATH . '/vendor/pocketframe/framework/src/functions.php';
 
 return [
-    date_default_timezone_set("Africa/Kampala"),
+    'timezone' => date_default_timezone_set("Africa/Kampala"),
 
     'app_key' => env('APP_KEY', 'your-secret-key'),
-    'env' => env('APP_ENV', 'production'),
-    'debug' => env('APP_DEBUG', false),
+    'env'     => env('APP_ENV', 'production'),
+    'debug'   => env('APP_DEBUG', false),
 
 ];
