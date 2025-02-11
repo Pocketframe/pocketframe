c23f15c1711edd5031021e528641d5dacd8f7aee
Asaba William <asabawilliamdk@yahoo.com>
2025-02-11 12:22:40 +0300

    refactor: bind more components to the container

--- bootstrap.php
+++ bootstrap.php
@@ -2,15 +2,42 @@
 
 use Pocketframe\Container\Container;
 use Pocketframe\Database\Database;
+use Pocketframe\Database\DB;
 use Pocketframe\Exceptions\Handler;
+use Pocketframe\Logger\Logger;
+use Pocketframe\Routing\Router;
 
 $container = new Container();
+$router = new Router($container);
+
+
+$middlewareConfig = require config_path('middleware.php');
+
+// Register global middleware
+foreach ($middlewareConfig['global'] as $middleware) {
+    $router->addGlobalMiddleware($middleware);
+}
+
+// Register middleware groups
+$router->group(['middleware' => $middlewareConfig['groups']['web']], function () use ($router) {
+    require routes_path('web.php');
+});
+
+$router->group(['middleware' => $middlewareConfig['groups']['api'], 'prefix' => 'api'], function () use ($router) {
+    require routes_path('api.php');
+});
 
 $container->bind('exceptionHandler', function () {
     return new Handler();
 });
 
-$container->bind('Core\Database\Database', function () {
-    $database = require base_path('config/database.php');
-    return new Database($database['database']);
+$container->bind(Database::class, function () {
+    $config = require base_path('config/database.php');
+    return new Database($config['database']);
 });
+
+$container->bind(Logger::class, function () {
+    return new Logger();
+});
+
+DB::setContainer($container);
