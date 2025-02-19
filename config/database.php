53ab8a95eb97d6debe27fd319f02fb081e6b4c0e
William Asaba <asabawilliamdk@yahoo.com>
2025-02-19 14:16:29 +0300

    refactor: modify

--- config/database.php
+++ config/database.php
@@ -1,28 +1,18 @@
 <?php
 require BASE_PATH . '/vendor/pocketframe/framework/src/functions.php';
-return [
-        'database' => [
-                'host' => 'localhost',
-                'port' => 3306,
-                'dbname' => 'pocketframe-demo',
-                'charset' => 'utf8mb4',
-                'username' => 'root',
-                'password' => 'admin@2564',
-        ],
-];
 
-// return [
-//         'database' => [
-//                 'host'           => env('DB_HOST', 'localhost'),
-//                 'port'           => env('DB_PORT', '3306'),
-//                 'dbname'       => env('DB_DATABASE'),
-//                 'username'       => env('DB_USERNAME'),
-//                 'password'       => env('DB_PASSWORD'),
-//                 'charset'        => 'utf8mb4',
-//                 'collation'      => 'utf8mb4_unicode_ci',
-//                 'prefix'         => '',
-//                 'prefix_indexes' => true,
-//                 'strict'         => true,
-//                 'engine'         => 'InnoDB',
-//         ],
-// ];
+return [
+  'database' => [
+    'host'           => env('DB_HOST', 'localhost'),
+    'port'           => env('DB_PORT', '3306'),
+    'dbname'       => env('DB_DATABASE'),
+    'username'       => env('DB_USERNAME'),
+    'password'       => env('DB_PASSWORD'),
+    'charset'        => 'utf8mb4',
+    'collation'      => 'utf8mb4_unicode_ci',
+    'prefix'         => '',
+    'prefix_indexes' => true,
+    'strict'         => true,
+    'engine'         => 'InnoDB',
+  ],
+];
