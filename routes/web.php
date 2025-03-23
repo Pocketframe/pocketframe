4c315e6d7ac5b5c1ff3b249d562950f8fd83bdae
William Asaba <asabawilliamdk@yahoo.com>
2025-03-23 23:40:26 +0300

    refactor: modify route files

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,15 @@
 
 /**
  * Web Routes
- * 
+ *
  * Define your web routes here. Routes are registered with the Router
  * and can be assigned middleware for request filtering.
- * 
+ *
  * Example:
- * $router->get('/path', 'Controller@method');
- * $router->post('/form', 'Controller@handle', [Middleware::class]);
+ * $router->get('/path', [Controller::class, 'method'], name: 'route-name');
  */
 
-use Pocketframe\Middleware\CsrfMiddleware;
+use App\Controllers\Web\HomeController;
 
-$router->get('/', 'App\Controllers\Web\HomeController@index');
 
-// $router->group(['middleware' => [CsrfMiddleware::class]], function ($router) {
-$router->get('/posts', 'App\Controllers\Web\Posts\PostsController@index');
-$router->get('/posts/create', 'App\Controllers\Web\Posts\PostsController@create');
-$router->post('/posts', 'App\Controllers\Web\Posts\PostsController@store');
-$router->get('/posts/{id}', 'App\Controllers\Web\Posts\PostsController@show');
-$router->get('/posts/{id}/edit', 'App\Controllers\Web\Posts\PostsController@edit');
-$router->put('/posts/{id}', 'App\Controllers\Web\Posts\PostsController@update');
-$router->delete('/posts/{id}', 'App\Controllers\Web\Posts\PostsController@destroy');
-// });
+$router->get('/', [HomeController::class, 'index'], name: 'home');
