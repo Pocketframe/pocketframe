a55f7c95fc195a4c13ee498cf07e1a775c02805e
williamDk <asabawilliamdk@yahoo.com>
2025-03-24 13:25:36 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
     'driver'         => env('DB_CONNECTION', 'sqlite'),
     'host'           => env('DB_HOST', 'localhost'),
     'port'           => env('DB_PORT', '3306'),
-    'database'       => env('DB_DATABASE', database_path('database.sqlite')),
+    'database'       => env('DB_DATABASE', 'pocketframe'),
     'username'       => env('DB_USERNAME', 'root'),
     'password'       => env('DB_PASSWORD', ''),
     'charset'        => 'utf8mb4',
